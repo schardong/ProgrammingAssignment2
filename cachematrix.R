2e8693ce931337c08e82362fb1d4eb5ff239b894
Guilherme G. Schardong <schardong.gg@gmail.com>
2016-10-10 22:50:31 -0300

    Finished the functions and added some basic documentation.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title Cache building function for the matrix inverse operation.
+##' @param x The matrix to be inverted. Default value is an empty matrix.
+##' @return A list of methods to enable the caching of the matrix inverse
+##' operation.
+##' @author Guilherme G. Schardong
+##' @examples
+##' A <- matrix(data = rnorm(1000000), nrow = 1000)
+##' mat.cache <- makeCacheMatrix(A)
 makeCacheMatrix <- function(x = matrix()) {
+    inverse <- NULL
 
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+
+    get <- function() x
+    setinverse <- function(i) inverse <<- i
+    getinverse <- function() inverse
+
+    list(set = set,
+         get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
+##' .. content for \description{} (no empty lines) ..
+##'
+##' .. content for \details{} ..
+##' @title Caching solution for the matrix inverse operation.
+##' @param x The matrix to be inverted.
+##' @param ... Other parameters for the \code{solve} function.
+##' @return Returns the matrix inverse of \code{x}.
+##' @author Guilherme G. Schardong
+##' @seealso \code{solve}
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getinverse()
+    if (!is.null(inverse)) {
+        message("Matrix inverse: Using cached data.")
+        return(inverse)
+    }
+
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
 }
